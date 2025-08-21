27d2ba06f983be9b18ba52b46149f2caa9759e1c
bracc <verabracco.official@gmail.com>
2025-08-21 21:36:37 +0200

    Changing menu interactions Creating ABC class Creating Factory Adding Level1 images level 1 Bg implementation

--- code/const.py
+++ code/const.py
@@ -2,11 +2,22 @@ C_WHITE = (255, 255, 255)
 C_ORANGE = (255, 128, 0)
 C_YELLOW = (255, 255, 0)
 
+
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+}
+
+
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
 
 WIN_WIDTH = 1000
 WIN_HEIGHT = 500
